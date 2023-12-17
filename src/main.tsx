1e756e4e0af64bd13f5fe5fddbaba7558d7535ec
Adrien Boutigny <adrien@slash-tmp.dev>
2023-12-17 21:12:47 +0100

    replace react-scripts with vite

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import { createRoot } from "react-dom/client";
+
+import "./index.css";
+import App from "./App";
+import { StrictMode } from "react";
+
+const root = createRoot(document.getElementById("root")!);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
